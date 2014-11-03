395c78f6f94a7bbe66dd29b7ab3e3474fdd50de3
威老 <qqq123026689@126.com>
2014-11-03 15:46:37 +0800

    refactor

--- tasks/staticfy.js
+++ tasks/staticfy.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
     var _ = require('underscore');
 
     grunt.registerMultiTask('staticfy', 'Staticfy your website', function () {
+        var inject_script;
         // Merge task-specific and/or target-specific options with these defaults.
         var gruntDone = _.after(this.files.length, grunt.task.current.async());
         var options = this.options({
@@ -32,43 +33,41 @@ module.exports = function (grunt) {
 
         // Convert to string, it would be used by phantomjs later.
         if (grunt.util.kindOf(options.inject_script) === 'function') {
-            options.inject_script = options.inject_script
+            inject_script = options.inject_script
                 .toString()
                 .replace(/(function \(\) \{([\w\W]*?)\})/, "$2")
                 .trim();
         }
 
         _.each(this.files, function (file, i) {
-            var fileSrc = file.src[0];
-            var wwwDir = options.cwd || path.dirname(fileSrc);
-            var fileBasePath = fileSrc.replace(wwwDir, '').replace(/\/$/, '');
-            grunt.log.writeln('File "' + fileBasePath + '" staticfying.');
+            var src, wwwDir, basePath, server, url;
 
-            if (!grunt.file.exists(fileSrc)) {
+            src = file.src[0];
+            wwwDir = options.cwd || path.dirname(src);
+            basePath = src.replace(wwwDir, '').replace(/\/$/, '');
+            grunt.log.writeln('File "' + basePath + '" staticfying.');
+
+            if (!grunt.file.exists(src)) {
                 // File not exist
-                grunt.log.warn('Source file "' + fileSrc + '" not found.');
+                grunt.log.warn('Source file "' + src + '" not found.');
                 return;
             }
 
             // Run a server to serve html files, we need a static server so we
             // wouldn't got a crossdomain error if the page use ajax or etc.
-            var server = SimpleServer.start(wwwDir, options.server_port + i);
+            server = SimpleServer.start(wwwDir, options.server_port + i);
 
-            var url = options.server_host + ':' + (options.server_port + i)
-                + '/' + fileBasePath;
-
+            url = options.server_host + ':' + (options.server_port + i) + '/' + basePath;
             if (options.query_string) url += '?' + options.query_string;
 
-
-            phantom(url, file.dest, options.inject_script, options.wait_request, function () {
+            phantom(url, file.dest, inject_script, options.wait_request, function () {
 
                 // After phantom, read the dest html file then normalizelf and make some changes.
                 var str = grunt.file.read(file.dest);
                 str = grunt.util.normalizelf(str);
                 str = options.onfinish(str);
-
-                // Write the result
                 grunt.file.write(file.dest, str);
+
                 grunt.log.writeln('File "' + file.dest + '" created.');
 
                 // Close the static Server
